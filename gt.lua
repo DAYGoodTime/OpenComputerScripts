90fed4be65c98e981c6ca00e291f4751185fb2ca
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2023-08-17 17:17:21 +0800

    update gt.lua

--- gt.lua
+++ gt.lua
@@ -4,7 +4,7 @@ Event = require("event")
 Gt = Component.gt_machine
 Redstone = Component.redstone
 Runing = false;
-CdTime = 30
+CdTime = 20
 CapacityHigh = 95
 CapacityLow = 30
 AcceptSide = 4;
@@ -17,20 +17,45 @@ local timer = Event.timer(CdTime, function()
         Runing = false;
     end
     if Runing then
-       local percent = Gt.getEUStored() / Gt.getEUMaxStored()
+       local percent_float = (Gt.getEUStored() / Gt.getEUMaxStored())
+       local percent = 0
+       if percent_float >=0.999 then
+        percent = 100
+        percent_float = 1;
+        else
+            percent = math.floor(percent_float*100)
+        end
+       local progressBar = {}
+       table.insert(progressBar,"[")
+       if percent==0 then
+        table.insert(progressBar,"0%")
+       end
+       for i=1,9 do
+            if i == 5 then
+                table.insert(progressBar,string.format("%.2f",percent_float*100) .. "%")
+            else
+                table.insert(progressBar,"=")
+            end
+       end
+       print(percent >= CapacityHigh)
        if percent >= CapacityHigh then
             Redstone.setOutput(OutputSide,0)
        end
        if percent <= CapacityLow then
             Redstone.setOutput(OutputSide,15)
        end
+       table.insert(progressBar,"]")
+       print("当前电池容量:".. table.concat(progressBar))
+       else
+        print("NOT RUNNING")
     end
 end, math.huge)
 
 while true do
-    local _,_,_,_,_, eventID = Event.pull()
+    local _, _, _, _, _, eventID = Event.pull()
+  
     if eventID == "interrupted" then
-        Event.cancel(timer)
-        break
+      Event.cancel(timer)
+      break
     end
 end
